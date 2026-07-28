368f8eb7447e3dfae1e8bf0cbf01e1ee1e2b7de8
KEEVIN MITCHELL <keevin.mitchell@gmail.com>
2026-07-28 19:48:07 -0400

    Use iTunes search terms for genre filter

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,25 +1,32 @@
-import { HiOutlineHashtag, HiOutlineHome, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import {
+  HiOutlineHashtag,
+  HiOutlineHome,
+  HiOutlinePhotograph,
+  HiOutlineUserGroup,
+} from "react-icons/hi";
 
+// value is passed straight to the iTunes search API as a term, so it has to
+// read like something a person would type - not a Shazam genre code.
 export const genres = [
-  { title: 'Pop', value: 'POP' },
-  { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
-  { title: 'Dance', value: 'DANCE' },
-  { title: 'Electronic', value: 'ELECTRONIC' },
-  { title: 'Soul', value: 'SOUL_RNB' },
-  { title: 'Alternative', value: 'ALTERNATIVE' },
-  { title: 'Rock', value: 'ROCK' },
-  { title: 'Latin', value: 'LATIN' },
-  { title: 'Film', value: 'FILM_TV' },
-  { title: 'Country', value: 'COUNTRY' },
-  { title: 'Worldwide', value: 'WORLDWIDE' },
-  { title: 'Reggae', value: 'REGGAE_DANCE_HALL' },
-  { title: 'House', value: 'HOUSE' },
-  { title: 'K-Pop', value: 'K_POP' },
+  { title: "Pop", value: "pop" },
+  { title: "Hip-Hop", value: "hip hop" },
+  { title: "Dance", value: "dance" },
+  { title: "Electronic", value: "electronic" },
+  { title: "Soul", value: "soul r&b" },
+  { title: "Alternative", value: "alternative" },
+  { title: "Rock", value: "rock" },
+  { title: "Latin", value: "latin" },
+  { title: "Film", value: "soundtrack" },
+  { title: "Country", value: "country" },
+  { title: "Worldwide", value: "world music" },
+  { title: "Reggae", value: "reggae dancehall" },
+  { title: "House", value: "house" },
+  { title: "K-Pop", value: "k-pop" },
 ];
 
 export const links = [
-  { name: 'Discover', to: '/', icon: HiOutlineHome },
-  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
-  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
-  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+  { name: "Discover", to: "/", icon: HiOutlineHome },
+  { name: "Around You", to: "/around-you", icon: HiOutlinePhotograph },
+  { name: "Top Artists", to: "/top-artists", icon: HiOutlineUserGroup },
+  { name: "Top Charts", to: "/top-charts", icon: HiOutlineHashtag },
 ];
